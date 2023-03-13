0978778cdb74b6b326bd8146342975cb2ad0cc19
Alexandr Chigrinets <alexandr@wps.su>
2023-03-13 23:06:54 +0300

    Updating dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,20 +3,20 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val bouncyCastle = "1.70"
+    val bouncyCastle = "1.72"
     val mouse = "1.2.1"
-    val sttp = "3.8.5"
+    val sttp = "3.8.13"
     val newtype = "0.4.4"
     val enumeratum = "1.7.2"
     val tofu = "0.11.1"
-    val circe = "0.14.3"
-    val fs2 = "3.4.0"
-    val scalapb = "0.11.12"
-    val grpc = "1.51.1"
+    val circe = "0.14.5"
+    val fs2 = "3.6.1"
+    val scalapb = "0.11.13"
+    val grpc = "1.53.0"
   }
 
-  val bcprov = "org.bouncycastle" % "bcprov-jdk15on" % Versions.bouncyCastle
-  val bcpkix = "org.bouncycastle" % "bcpkix-jdk15on" % Versions.bouncyCastle
+  val bcprov = "org.bouncycastle" % "bcprov-jdk18on" % Versions.bouncyCastle
+  val bcpkix = "org.bouncycastle" % "bcpkix-jdk18on" % Versions.bouncyCastle
   val mouse = "org.typelevel" %% "mouse" % Versions.mouse
   val sttpCore = "com.softwaremill.sttp.client3" %% "core" % Versions.sttp
   val newtype = "io.estatico" %% "newtype" % Versions.newtype
